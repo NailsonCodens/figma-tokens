1b17f0bfb454efad6a1cf3096fe6c8db4a68d919
NailsonCodens <NailsonCodens@users.noreply.github.com>
2022-10-19 03:35:53 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,11 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Wed, 19 Oct 2022 03:31:26 GMT
+ * Generated on Wed, 19 Oct 2022 03:35:52 GMT
  */
 
 module.exports = {
   "ColorWhite": "#ffffff",
-  "ColorPurple900": "#8257e6",
+  "ColorPurple600": "#8257e6",
+  "ColorPurple900": "#4e338d",
   "ColorGray500": "#3d3c3c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214"
