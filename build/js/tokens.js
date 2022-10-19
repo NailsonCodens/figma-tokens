c1e946d48f3baaef0bcf8e7e42cde9194abe0a09
nailson <nailson.ivs@codens.com.br>
2022-10-19 00:04:09 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 19 Oct 2022 01:39:39 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple900": "#8257e6",
+  "ColorGray500": "#3d3c3c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214"
+};
